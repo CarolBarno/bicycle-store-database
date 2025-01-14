2bd55b7b3882f3876c58081a73acc0628ae305d0
CarolBarno <carolbarno8@gmail.com>
2025-01-14 20:40:34 +0300

    second commit

--- scripts/load_data.py
+++ scripts/load_data.py
@@ -1,11 +1,15 @@
+import os
+from dotenv import load_dotenv
 import pandas as pd
 from sqlalchemy import create_engine
 
-db_user = "postgres"
-db_password = "admin"
-db_host = "localhost"
-db_port = "5432"
-db_name = "bicycle_store"
+load_dotenv()
+
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASSWORD")
+db_host = os.getenv("DB_HOST")
+db_port = os.getenv("DB_PORT")
+db_name = os.getenv("DB_NAME")
 
 # Create an engine for PostgreSQL
 engine = create_engine(
@@ -17,7 +21,7 @@ engine = create_engine(
 #     f'mysql+pymysql://{db_user}:{db_password}@{db_host}/{db_name}')
 
 
-def load_csv_to_db(file_path, table_name, columns, unique_key=None, date_columns=None, filter_column=None, reference_table=None):
+def load_csv_to_db(file_path, table_name, columns, unique_key=None, date_columns=None, filter_column=None, reference_table=None, if_exists='append'):
     try:
         data = pd.read_csv(file_path, usecols=lambda x: x.lower() in [col.lower() for col in columns])
         data.columns = [col.lower() for col in data.columns]
@@ -28,14 +32,13 @@ def load_csv_to_db(file_path, table_name, columns, unique_key=None, date_columns
         if date_columns:
             for date_column in date_columns:
                 data[date_column] = pd.to_datetime(data[date_column], format='%d-%m-%Y', errors='coerce')
+            data = data.dropna(subset=date_columns)
 
         if filter_column and reference_table:
-            existing_ids = pd.read_sql(f"SELECT customer_id FROM {reference_table}", con=engine)
-            data = data[data[filter_column].isin(existing_ids['customer_id'])]
+            existing_ids = pd.read_sql(f"SELECT DISTINCT {filter_column} FROM {reference_table}", con=engine)
+            data = data[data[filter_column].isin(existing_ids[filter_column])]
 
-        data = data.dropna(subset=date_columns)
-
-        data.to_sql(table_name, con=engine, if_exists='append', index=False)
+        data.to_sql(table_name, con=engine, if_exists=if_exists, index=False)
         print(f"Data loaded successfully into {table_name}")
     except Exception as e:
         print(f"Error loading data into {table_name}: {e}")
